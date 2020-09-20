1e01da95ebb5c0d87ba550196bdba8d07757db4b
Kalick153 <71277325+Kalick153@users.noreply.github.com>
2020-09-20 22:59:09 +0300

    Add files via upload

---
+++ HW1Task1code.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+b = 10
+# a = np.log(11/10)
+# for i in range(1, 26):
+#     a = 1/i - b * a
+
+# a = 0
+# for i in range(25):
+#     a = 1/(b * (50 - i)) - a/b
+#
+#
+# print(a)
